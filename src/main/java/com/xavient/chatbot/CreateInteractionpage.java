843b554bb7ae77bec7029dcae309e0e0533badb9
Sandeep <smaddur@xavient.com>
2019-02-13 13:22:43 -0500

    CreateInteractionpage

--- src/main/java/com/xavient/chatbot/CreateInteractionpage.java
+++ src/main/java/com/xavient/chatbot/CreateInteractionpage.java
@@ -10,95 +10,97 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CreateInteractionpage {
-private WebDriver driver;
+	private WebDriver driver;
 
-    private static WebElement element = null;
+	private static WebElement element = null;
 
-    @FindBy(xpath="//ul[@class='nav-link']//li[4]")
-    public  WebElement centralRepoimage;
-	
+	@FindBy(xpath="//ul[@class='nav-link']//li[4]")
+	public  WebElement centralRepoimage;
+
 	@FindBy(xpath = "//a[@title='FLOW']")
 	public  WebElement flowBtn;
-	
+
 	@FindBy(xpath="//span[@title='QA Onsite Automation']")
 	public WebElement folderQAautomation;
 
 	@FindBy(xpath="//span[contains(text(),'New Interaction')]")
 	public WebElement newInteraction;
-	
+
 	@FindBy(xpath="//input[@placeholder='Enter template name']")
 	public WebElement templateName;
-	
+
 	@FindBy(xpath="//button[contains(text(),'Ok')]")
 	public WebElement okBtn;
-	
+
 	@FindBy(xpath="//div[@class='add-btn']")
 	public WebElement addBtn;
-	
+
 	@FindBy(xpath="//input[@placeholder='Enter template title here']")
 	public WebElement templateTitle;
-	
-	
+
+
 	@FindBy(xpath="//textarea[@placeholder='Add Question here']")
 	public WebElement addQuestion;
-	
+
 	@FindBy(xpath="//div[contains(text(),'Text')]")
+	public WebElement textBtn;
+
+	@FindBy(xpath="//div[@class='ql-editor ql-blank']//p")
 	public WebElement textAnswer;
-	
+
 	@FindBy(xpath="//span[contains(text(),'Save')]")
 	public WebElement saveBtn;
 
-public CreateInteractionpage(WebDriver driver)
+	public CreateInteractionpage(WebDriver driver)
 
-{
+	{
 
-this.driver = driver;
+		this.driver = driver;
 //Initialise Elements
-PageFactory.initElements(driver, this);
+		PageFactory.initElements(driver, this);
 
-}
-
-public void clickCentralbtn(){
-	WebDriverWait wait = new WebDriverWait(driver, 200);
-	wait.until(ExpectedConditions.elementToBeClickable(centralRepoimage)).click();
-	
-}
-
-public void clickFlowbtn() {
-	Actions actions = new Actions(driver);
-	actions.moveToElement(flowBtn).click().perform();
-}
-
-public void clickFolder() {
-	Actions actions = new Actions(driver);
-	actions.moveToElement(folderQAautomation);	
-	
-	actions.click().build().perform();
-	
-	
 	}
 
-public void clickNewinteraction() {
-	Actions actions = new Actions(driver);
-	actions.moveToElement(newInteraction);
-	actions.click().build().perform();
-	
+	public void clickCentralbtn(){
+		WebDriverWait wait = new WebDriverWait(driver, 200);
+		wait.until(ExpectedConditions.elementToBeClickable(centralRepoimage)).click();
+
+	}
+
+	public void clickFlowbtn() {
+		Actions actions = new Actions(driver);
+		actions.moveToElement(flowBtn).click().perform();
+	}
+
+	public void clickFolder() {
+		Actions actions = new Actions(driver);
+		actions.moveToElement(folderQAautomation);
+
+		actions.click().build().perform();
+
+
+	}
+
+	public void clickNewinteraction() {
+		Actions actions = new Actions(driver);
+		actions.moveToElement(newInteraction);
+		actions.click().build().perform();
+
+	}
+
+	public void clickOkBtn()
+	{
+		Actions actions = new Actions(driver);
+		actions.moveToElement(okBtn);
+		actions.click().build().perform();
+
+	}
+
+	public void clickaddBtn() throws InterruptedException{
+		Actions actions = new Actions(driver);
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//div[@class='add-btn']")).click();
+
+	}
+
 }
-
-public void clickOkBtn()
-{
-	Actions actions = new Actions(driver);
-	actions.moveToElement(okBtn);
-	actions.click().build().perform();
-	
-}
-
-public void clickaddBtn() throws InterruptedException{
-	Actions actions = new Actions(driver);
-	Thread.sleep(3000);
-	driver.findElement(By.xpath("//div[@class='add-btn']")).click();
-
-}
-
-}
-
--- src/test/java/com/xavient/chatbot/CreateInteractionTest.java
+++ src/test/java/com/xavient/chatbot/CreateInteractionTest.java
@@ -10,53 +10,55 @@ import java.util.concurrent.TimeUnit;
 
 public class CreateInteractionTest  extends BaseE2ETest{
 
-	public WebDriver driver;
+    public WebDriver driver;
 
-	@Before
+    @Before
 
-	public void setup() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		final String endPoint = testProperties.get("endPoint");
-		driver.get(testProperties.get("endPoint"));
-		driver.manage().window().maximize();
-		ExcelDataConfig excel = new ExcelDataConfig((testProperties.get("excelPath")));
-		Loginpage login = new Loginpage();
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        final String endPoint = testProperties.get("endPoint");
+        driver.get(testProperties.get("endPoint"));
+        driver.manage().window().maximize();
+        ExcelDataConfig excel = new ExcelDataConfig((testProperties.get("excelPath")));
+        Loginpage login = new Loginpage();
 
-		System.out.println(excel.getdata(1, 1, 0));
-		System.out.println(excel.getdata(1, 1, 1));
+        System.out.println(excel.getdata(1, 1, 0));
+        System.out.println(excel.getdata(1, 1, 1));
 
-		login.txtbx_UserName(driver).sendKeys(excel.getdata(1, 1, 0));
+        login.txtbx_UserName(driver).sendKeys(excel.getdata(1, 1, 0));
 
-		Loginpage.txtbx_Password(driver).sendKeys(excel.getdata(1, 1, 1));
+        Loginpage.txtbx_Password(driver).sendKeys(excel.getdata(1, 1, 1));
 
-		Loginpage.btn_LogIn(driver).click();
-	}
-	
-	//This is to create a new interaction flow
-	@Test
-	public void createinteraction() {
-		try {
-			CreateInteractionpage interact = new CreateInteractionpage(driver);
-			ExcelDataConfig excel = new ExcelDataConfig((testProperties.get("excelPath")));
-			interact.clickCentralbtn();
-			driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
-			interact.clickFlowbtn();
-			
-			//Clicking on the folder QA Automation onsite.(As of now its not happening properly)
-			interact.clickFolder();
-			//As of now its clicking the New interaction from the root folder and not under the created folder QA Automation Onsite.
-		    interact.clickNewinteraction();
-			interact.templateName.sendKeys(excel.getdata(1, 1, 7));
-			interact.clickOkBtn();
-			interact.clickaddBtn();
-		    
-			interact.addQuestion.sendKeys(excel.getdata(1, 1, 9));
-			interact.textAnswer.sendKeys(excel.getdata(1, 1, 10));
-			interact.templateTitle.sendKeys(excel.getdata(1, 1, 8));
-}
-		catch (Exception e) {
-			System.out.println("exception is" + e);
-		}
-	}
+        Loginpage.btn_LogIn(driver).click();
+    }
+
+    //This is to create a new interaction flow
+    @Test
+    public void createinteraction() {
+        try {
+            CreateInteractionpage interact = new CreateInteractionpage(driver);
+            ExcelDataConfig excel = new ExcelDataConfig((testProperties.get("excelPath")));
+            interact.clickCentralbtn();
+            driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+            interact.clickFlowbtn();
+
+            //Clicking on the folder QA Automation onsite.(As of now its not happening properly)
+            interact.clickFolder();
+            //As of now its clicking the New interaction from the root folder and not under the created folder QA Automation Onsite.
+            interact.clickNewinteraction();
+            interact.templateName.sendKeys(excel.getdata(1, 1, 7));
+            interact.clickOkBtn();
+            interact.clickaddBtn();
+
+            interact.addQuestion.sendKeys(excel.getdata(1, 1, 9));
+            interact.textBtn.click();
+            interact.textAnswer.sendKeys(excel.getdata(1, 1, 10));
+            interact.templateTitle.sendKeys(excel.getdata(1, 1, 8));
+            interact.saveBtn.click();
+        }
+        catch (Exception e) {
+            System.out.println("exception is" + e);
+        }
+    }
 }
