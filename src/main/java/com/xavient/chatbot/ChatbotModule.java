8bbf2623b52baa8f750500b803b13cb92d14938e
Sandeep <smaddur@xavient.com>
2019-02-19 10:10:35 -0500

    ChatBotModule

---
+++ src/main/java/com/xavient/chatbot/ChatbotModule.java
@@ -0,0 +1,75 @@
+package com.xavient.chatbot;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ChatbotModule {
+
+    private WebDriver driver;
+
+    private static WebElement element = null;
+
+    @FindBy(xpath = "//a[@title='Chatbots']//img")
+    public WebElement chatBot;
+
+   @FindBy(xpath = "//a[@title='QA Bot ']")
+    public WebElement qaBotBtn;
+
+   @FindBy(xpath = "//span[text()='Import']//parent::button")
+    public WebElement importBtn;
+
+    @FindBy(xpath = "//span[@title='Zynga']")
+    public WebElement QAFolder;
+
+    @FindBy(xpath = "//li[@title='Feedback']")
+    public WebElement feedback;
+
+    @FindBy(xpath = "//div[text()='Templates imported successfully in bot category.']")
+    public WebElement importSuccessMsg;
+
+    @FindBy(xpath = "//span[text()='Train Bot']//parent::button")
+    public WebElement trainBot;
+
+    @FindBy(xpath = "//div[text()='Train job has been scheduled.']")
+    public WebElement trainBotSuccessMsg;
+
+    public ChatbotModule(WebDriver driver)
+    {
+        this.driver = driver;
+        //Initialise Elements
+        PageFactory.initElements(driver, this);
+    }
+
+    public void clickChatBotbtn() {
+        WebDriverWait wait = new WebDriverWait(driver, 200);
+        wait.until(ExpectedConditions.elementToBeClickable(chatBot)).click();
+
+    }
+
+    public void clickQABotbtn() {
+        Actions actions = new Actions(driver);
+        actions.moveToElement(qaBotBtn).click().perform();
+    }
+    public void clickImport(){
+        WebDriverWait wait = new WebDriverWait(driver, 200);
+        wait.until(ExpectedConditions.elementToBeClickable(importBtn)).click();
+
+    }
+    public void selectFolder(){
+       QAFolder.click();
+        WebDriverWait wait = new WebDriverWait(driver, 200);
+        wait.until(ExpectedConditions.elementToBeClickable(feedback)).click();
+    }
+    public void verifyMsg(){
+        importSuccessMsg.isDisplayed();
+        WebDriverWait wait = new WebDriverWait(driver, 200);
+        wait.until(ExpectedConditions.elementToBeClickable(trainBot)).click();
+        trainBotSuccessMsg.isDisplayed();
+    }
+
+}
---
+++ src/test/java/com/xavient/chatbot/ChatBotModuleTest.java
@@ -0,0 +1,61 @@
+package com.xavient.chatbot;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ChatBotModuleTest extends BaseE2ETest {
+
+    public WebDriver driver;
+
+    @Before
+
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        final String endPoint = testProperties.get("endPoint");
+        driver.get(testProperties.get("endPoint"));
+        driver.manage().window().maximize();
+        ExcelDataConfig excel = new ExcelDataConfig((testProperties.get("excelPath")));
+        Loginpage login = new Loginpage();
+
+        System.out.println(excel.getdata(1, 1, 0));
+        System.out.println(excel.getdata(1, 1, 1));
+
+        login.txtbx_UserName(driver).sendKeys(excel.getdata(1, 1, 0));
+
+        Loginpage.txtbx_Password(driver).sendKeys(excel.getdata(1, 1, 1));
+
+        Loginpage.btn_LogIn(driver).click();
+
+    }
+
+    @Test
+  public void ChatBot() {
+        try {
+            ChatbotModule chat = new ChatbotModule(driver);
+            ExcelDataConfig excel = new ExcelDataConfig((testProperties.get("excelPath")));
+            driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+            Thread.sleep(2000);
+            chat.clickChatBotbtn();
+            Thread.sleep(2000);
+            chat.clickQABotbtn();
+            Thread.sleep(2000);
+            chat.clickImport();
+            Thread.sleep(2000);
+            chat.selectFolder();
+            Thread.sleep(2000);
+            chat.clickImport();
+            chat.verifyMsg();
+
+
+
+        } catch (Exception e) {
+            System.out.println("exception is" + e);
+        }
+    }
+}
